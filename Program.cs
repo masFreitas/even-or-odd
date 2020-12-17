f7c82412c15ca647be6b3e61248faf91dd2bc32b
Mateus Arthur da Silva de Freitas <65245536+masFreitas@users.noreply.github.com>
2020-12-16 21:53:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercicioUm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+            Console.WriteLine("informe um numero: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            if (n1 % 2 == 0)
+            {
+                Console.WriteLine("É par");
+            }
+            else
+            {
+                Console.WriteLine("É impar");
+            }
+        }
+    }
+}
